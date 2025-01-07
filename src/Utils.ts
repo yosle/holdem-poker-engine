cdbe24409c828f116d537c3dcfc913f11d06e36a
yosle <yosle007@gmail.com>
2025-01-07 03:45:28 -0500

    not using log transport to file

--- src/Utils.ts
+++ src/Utils.ts
@@ -11,6 +11,6 @@ export const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    // new winston.transports.File({ filename: "logs/combined.log" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
   ],
 });
